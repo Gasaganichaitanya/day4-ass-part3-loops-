73fdf8bcf8288d2cf7b2f3fb353f717412f7c7eb
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-09 15:29:34 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp_ass5_part3_Loops_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 1;
+            Console.WriteLine("First ten fibonacci numbers");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            for (int i = 0; i <= 7; i++)
+            {
+                int z = x + y;
+                Console.WriteLine(z);
+                x = y;
+                y = z;
+            }
+            Console.ReadKey();
+        }
+    }
+}
